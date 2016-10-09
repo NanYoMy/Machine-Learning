8466c8c41716acf07a28ab983d1e7132a3754a96
ospiro <Oliver@Olivers-MacBook-Air.local>
2016-10-09 00:30:14 -0400

    Changed eval function in indexed_function class

--- Elimination.py
+++ Elimination.py
@@ -1,5 +1,5 @@
 import operator
-from functools import partial
+import numpy as np
 
 
 
@@ -35,12 +35,24 @@ class indexed_function: #simple class containing function and its node dependenc
 	def __init__(self, deplist, prob):
 		self.deplist = deplist
 		self.prob = prob
-	def eval(*args):
-		return prob(args)
+	def marginalize(self,varindex):
+		self.prob = 
 
-def m_eval(func,depnode,val):
-	varname = "x" + string(depnode)
-	return partial(func, varname=val)
+	def eval(self,*args):
+		return self.prob[tuple(args)]
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def DAG_Eliminate():
 	order  = [4,3,2,1]
@@ -66,35 +78,10 @@ def DAG_Eliminate():
 			m.append(prodsum)
 			activelist.append(prodsum)
 
-#My sad little hard-coded-but-functional code
-def DAG_Eliminate():
-	def m4(x3):
-		return cond43(1,x3)
-	def m3(x2):
-		return cond32(0,x2)*m4(0)+cond32(1,x2)*m4(1)
-	def m2(x1):
-		return marg_x1(x1)*cond21(0,x1)*m3(0)+marg_x1(x1)*cond21(1,x1)*m3(1)
-	m1 = marg_x1(0)*m2(0)+marg_x1(1)*m2(1)
-	res = [m1,m2,m3,m4]
-	return res
-result = DAG_Eliminate()
-#p(x1=0,x4=1) for x4=1 is result[1](0)
-#p(x4) for x4=1 is result[0]
-#so p(x1=0 | x4 =1) is result[1](0)/result[0] 
 
 
 
-# def divide(n,m):
-# 	return n/m
-# def half():
-# 	return partial(divide,m=2)
-# print(partial(divide,m=2)(10))
-# def quarter():
-# 	return partial(divide,m=4)
-# a = [half,quarter]
-# def eighth():
-# 	return functools.reduce(operator.mul,a)
-# eighth()
+
 
 # def DAG_Eliminate():
 # 	order = [4,3,2,1]
